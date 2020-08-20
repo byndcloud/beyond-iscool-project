27d4dddbc2ac98d6faa6bcdec73cecb5fb3b2dbb
gui555pl <gpl2@cin.ufpe.br>
2020-08-20 18:55:00 -0300

    feat: fim da 1a aula

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- Without vuetify -->
+    <div class='toolbar'>Toolbar</div>
+    Conteúdo
+    <div class='bottom-bar'>
+      <input type='text'>
+      <button>Enviar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+  .toolbar {
+    height: 76px;
+    width: 100%;
+    background-color: #18FFFF;
+    display: flex;
+    align-items: center;
+    padding-left: 8px;
+  }
+  .bottom-bar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 76px;
+    width: 100%;
+    background-color: #29B6F6;
+    display: flex;
+    align-items: center;
+    padding-left: 8px;
+  }
+  .bottom-bar button, input {
+    background-color: #E0E0E0 !important;
+    margin: 4px;
+    padding: 4px;
+    border-radius: 4px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import App2 from './App2.vue'
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App2)
+}).$mount('#app')
