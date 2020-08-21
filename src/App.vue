b063134cd74221fbf46290f192ba51ffddbca64a
Arthur Lapprand <lapprand@gmail.com>
2020-08-21 14:32:46 -0300

    refactor: clean code

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,62 @@
 <template>
-  <div>
-    <!-- Without vuetify -->
-    <div class='toolbar'>Toolbar</div>
-    Conteúdo
-    <div class='bottom-bar'>
-      <input type='text'>
-      <button>Enviar</button>
-    </div>
-  </div>
+  <v-app>
+    <v-main>
+      <v-app-bar fixed>
+        <v-btn x-large icon>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{ name }}</v-toolbar-title>
+      </v-app-bar>
+      <div class="messages-container">
+        <div v-for="(message, i) in messages" :key="i">
+          <v-card outlined v-if="message.user == 'friend'" class='ma-3 teal accent-4'>
+            <v-card-subtitle class="font-weight-bold">{{ name }}</v-card-subtitle>
+            <v-card-text>{{ message.text}}</v-card-text>
+          </v-card>
+          <v-card outlined v-else class='ma-3 grey lighten-2'>
+            <v-card-subtitle class="font-weight-bold">Eu</v-card-subtitle>
+            <v-card-text>{{ message.text}}</v-card-text>
+          </v-card>
+        </div>
+      </div>
+      <v-app-bar color="#424242" fixed bottom>
+        <v-text-field v-model='field' @keyup.enter='send()' solo label="Mensagem..." hide-details></v-text-field>
+        <v-btn @click='send()' icon color="blue">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
-    //
+    name: "Roberto Carlos",
+    messages: [
+      { user: "friend", text: "Olá, pode me ajudar?" },
+      { user: "friend", text: "Sei que não falo contigo há bastante tempo mas estou escrevendo bastante aqui para termos um exemplo real de mensagem." }
+    ],
+    field: ''
   }),
+  methods: {
+    send () {
+      let message = {
+        user: 'me',
+        text: this.field
+      }
+      this.messages.push(message)
+      this.field = ''
+    }
+  }
 };
 </script>
 
 <style>
-  .toolbar {
-    height: 76px;
-    width: 100%;
-    background-color: #18FFFF;
-    display: flex;
-    align-items: center;
-    padding-left: 8px;
-  }
-  .bottom-bar {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 76px;
-    width: 100%;
-    background-color: #29B6F6;
-    display: flex;
-    align-items: center;
-    padding-left: 8px;
-  }
-  .bottom-bar button, input {
-    background-color: #E0E0E0 !important;
-    margin: 4px;
-    padding: 4px;
-    border-radius: 4px;
-  }
+.messages-container {
+  width: 100%;
+  padding-top: 64px;
+  padding-bottom: 56px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-// import App from './App.vue'
-import App2 from './App2.vue'
-import vuetify from './plugins/vuetify';
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App2)
+  render: h => h(App)
 }).$mount('#app')
