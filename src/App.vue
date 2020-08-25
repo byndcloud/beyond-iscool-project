805122d345ccd4e19d9fc7e267bd196517941623
gui555pl <gpl2@cin.ufpe.br>
2020-08-25 14:31:24 -0300

    fix: updates da aula #4

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,60 @@
 <template>
-  <div>
-    <!-- Without vuetify -->
-    <div class='toolbar'>Toolbar</div>
-    Conteúdo
-    <div class='bottom-bar'>
-      <input type='text'>
-      <button>Enviar</button>
-    </div>
-  </div>
+  <v-app>
+    <!-- With vuetify -->
+    <v-main>
+      <v-app-bar fixed>
+        <v-btn x-large icon>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{ name }}</v-toolbar-title>
+      </v-app-bar>
+      <div class="messages-container">
+        <div v-for="(message, i) in messages" :key="i">
+          <MessageCard :nameProp='name' :messageProp='message' />
+        </div>
+      </div>
+      <v-app-bar color="#424242" fixed bottom>
+        <v-text-field v-model='field' @keyup.enter='send()' solo label="Mensagem..." hide-details></v-text-field>
+        <v-btn @click='send()' icon color="blue">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
+import MessageCard from './components/MessageCard.vue'
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    MessageCard
+  },
   data: () => ({
-    //
+    name: "Roberto Carlos",
+    messages: [
+      { user: "friend", text: "Olá, pode me ajudar?" },
+      { user: "friend", text: "Sei que não falo contigo há bastante tempo mas estou escrevendo bastante aqui para termos um exemplo real de mensagem." }
+    ],
+    field: ''
   }),
+  methods: {
+    send () {
+      let message = {
+        user: 'me',
+        text: this.field
+      }
+      this.messages.push(message)
+      this.field = ''
+    }
+  }
 };
 </script>
 
 <style>
-  .toolbar {
-    height: 76px;
-    width: 100%;
-    background-color: #18FFFF;
-    display: flex;
-    align-items: center;
-    padding-left: 8px;
-  }
-  .bottom-bar {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 76px;
-    width: 100%;
-    background-color: #29B6F6;
-    display: flex;
-    align-items: center;
-    padding-left: 8px;
-  }
-  .bottom-bar button, input {
-    background-color: #E0E0E0 !important;
-    margin: 4px;
-    padding: 4px;
-    border-radius: 4px;
-  }
+.messages-container {
+  width: 100%;
+  padding-top: 64px;
+  padding-bottom: 56px;
+}
 </style>
